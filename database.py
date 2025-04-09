43d0009a7e9b09ba46f39c17c5879a70a5d20a6b
rafaelleeto <leetobr@gmail.com>
2025-04-09 17:12:34 -0300

    Função que cria o banco de dados e cadastra usuarios

--- database.py
+++ database.py
@@ -1,5 +1,25 @@
 import sqlite3
 
-# PARTE PRINCIPAL DO PROGRAMA
-if __name__ == '__main__':
-    print("Hello, world!")
+def conectar_banco():
+    conexao=sqlite3.connect("musica.db")
+    return conexao
+
+def criar_tabelas():
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (email TEXT PRIMARY KEY,nome TEXT , senha TEXT )''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS musicas (id INTEGER PRIMARY KEY, artista TEXT,letra TEXT, 
+                   email_usuario text, FOREIGN KEY (email_usuario) REFERENCES usuario(email))''')
+    conexao.commit()
+    cursor.close()
+
+def cadastro(email,nome,senha):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''INSERT INTO usuarios (email,nome,senha) VALUES (?,?,?)''',(email,nome,senha))
+    conexao.commit()
+    cursor.close()
+    
+    
+if __name__=="__main__":
+    criar_tabelas()
