b579478391bdbfb824397c6c0039ffc3bee2bad4
leetobr <leetobr@gmail.com>
2025-04-14 17:08:24 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
+import os
+import hashlib
 from flask import Flask, render_template, request, url_for, redirect, flash,session
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+import database
+
+BUF_SIZE = 65536
+
+UPLOAD_FOLDER = os.path.join("static","uploads")
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 app.secret_key = "chave_muito_segura"
-import database
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/') #rota para a página inicial
 def index():
@@ -20,7 +36,7 @@ def cadastro():
     nome=request.form['nome']
     senha=generate_password_hash(request.form['senha'])
     database.cadastro(email,nome,senha)
-    return redirect(url_for('login'))
+    return redirect('/login')
 
 @app.route('/login',methods=["GET","POST"])
 def login():
@@ -35,39 +51,67 @@ def login():
     if not check_password_hash(listadobanco[0],senha):
         return 'pode nao'
     session['email']=request.form['email']
-    return redirect(url_for('home'))
+    return redirect('/home')
 
 @app.route('/home',methods=["GET"])
 def home():
     musicas=database.pegar_musicas(session['email'])
-    return render_template('home.html',musicas=musicas)
+    return render_template('home.html',musicas=musicas,session=session)
 
 @app.route('/novo',methods=["GET","POST"])
 def novo():
     if request.method=="GET":
-        return render_template ('novo.html',)
+        return render_template ('novo.html')
+    file = request.files['imagem']
+    
+    if file and allowed_file(file.filename):
+        sha256 = hashlib.sha256()
+        while True:
+            dados = file.stream.read(BUF_SIZE)
+            if not dados:
+                break
+            sha256.update(dados)
+        # filename = secure_filename(file.filename)
+        file.stream.seek(0)
+        filename = sha256.hexdigest()
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     nome=request.form['nome']
     artista=request.form['artista']
     letra=request.form['letra']
     status=request.form['status']
-    database.adicionar_musica(nome,artista,status,letra,session['email'])
-    return redirect(url_for('home'))
+    database.adicionar_musica(nome,artista,status,letra,session['email'],filename)
+    return redirect('/home')
     
-
+@app.route('/editar_musica/<int:id>',methods=["GET","POST"])
+def editar(id):
+    if request.method=="GET":
+        if session['email']==database.pegar_musica(id)[5]:        
+            musica=database.pegar_musica(id)
+            return render_template('editar_musica.html',musica=musica)
+        return "Você está tentando acessar a edição de uma musica que não é sua!"
+    if session['email']==database.pegar_musica(id)[5]:
+        nome=request.form['nome']
+        artista=request.form['artista']
+        letra=request.form['letra']
+        status=request.form['status']
+        database.atualizar_musica(nome,artista,status,letra,id)
+        return redirect('/home')
+    return "Você está tentando editar uma música que não é da sua conta"
     
-   
+@app.route('/excluir_musica/<id>',methods=["GET"])
+def excluir_musica(id):
+    if session['email']==database.pegar_musica(id)[5]:
+        database.deletar_musica(id)
+        return redirect('/home')
+    return "Você está tentando deletar uma música que não é da sua conta"
     
+        
 
-
-
-
-
-
-
-
-
-
-
+@app.route("/excluir_conta",methods=["GET"])
+def excluir_conta():
+    email=session['email']
+    database.excluir_conta(email)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -10,7 +10,7 @@ def criar_tabelas():
     cursor = conexao.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (email TEXT PRIMARY KEY,nome TEXT , senha TEXT )''')
     cursor.execute('''CREATE TABLE IF NOT EXISTS musicas (id INTEGER PRIMARY KEY,nome TEXT, artista TEXT, status TEXT,
-                   letra TEXT, email_usuario text, FOREIGN KEY (email_usuario) REFERENCES usuario(email))''')
+                   letra TEXT, email_usuario TEXT, capa TEXT,  FOREIGN KEY (email_usuario) REFERENCES usuario(email))''')
     conexao.commit()
     cursor.close()
 
@@ -33,14 +33,14 @@ def verificar_se_usuario_existe(email):
     cursor = conexao.cursor()
     cursor.execute('''SELECT * FROM usuarios WHERE email=?''',(email,))
     conexao.commit()
-    return cursor.fetchall()
+    return cursor.fetchone()
 
-def adicionar_musica(nome,artista,status,letra,email):
+def adicionar_musica(nome,artista,status,letra,email,filename):
     conexao = conectar_banco()
     cursor = conexao.cursor()
     if nome=="1" and artista=="1" and status=="1" and letra=="1":
         return "pode nao"
-    cursor.execute('''INSERT INTO musicas (nome,artista,status,letra,email_usuario) VALUES(?,?,?,?,?)''',(nome,artista,status,letra,email))
+    cursor.execute('''INSERT INTO musicas (nome,artista,status,letra,email_usuario,capa) VALUES(?,?,?,?,?,?)''',(nome,artista,status,letra,email,filename))
     conexao.commit()
     cursor.close()
 
@@ -50,6 +50,37 @@ def pegar_musicas(email):
     cursor.execute('''SELECT * FROM musicas WHERE email_usuario=?''',(email,))
     conexao.commit()
     return cursor.fetchall()
+    
+def pegar_musica(id):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''SELECT * FROM musicas WHERE id=?''',(id,))
+    conexao.commit()
+    return cursor.fetchone()
+
+
+def atualizar_musica(nome,artista,status,letra,id):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    print(id)
+    cursor.execute('''UPDATE musicas SET nome=? , artista=? , status=?, letra=? WHERE id=? ''',(nome,artista,status,letra,id))
+    conexao.commit()
+    cursor.close()
+
+def deletar_musica(id):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''DELETE FROM musicas WHERE id=? ''',(id,))
+    conexao.commit()
+    cursor.close()
+    
+def excluir_conta(email):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''DELETE FROM musicas WHERE email_usuario=? ''',(email,))
+    cursor.execute('''DELETE FROM usuarios WHERE email=? ''',(email,))
+    conexao.commit()
+    cursor.close()
     
 
     
