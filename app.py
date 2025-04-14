780ada8c3977cd43522a9e3a1fed0953157499b1
leetobr <leetobr@gmail.com>
2025-04-14 02:35:22 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
+from flask import Flask, render_template, request, url_for, redirect, flash,session
 from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.secret_key = "chave_muito_segura"
@@ -8,48 +8,66 @@ import database
 def index():
     return render_template('index.html')
 
-@app.route('/login') #rota para a página de login
-def login():
-    return render_template('login.html')
-
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-
-# VERFIFICAR O LOGIN
-@app.route('/verificar-login', methods=['POST'])
-def verificar_login():
-# Pegando o que o usuário digitou no campo de entrada de user e senha
-    username = request.form['username']
-    password = request.form['password']
-
-    # Verifica se o usuario digitado está na lista e se 
-    # a senha está certa
-    if username in usuarios and usuarios[username] == password:
-        return redirect(url_for('home'))
-    else:
-        # Flash envia mensagem para o front-end
-        flash('Usuário ou senha incorretos', 'danger')
-        return redirect(url_for('login'))
-
 @app.route('/cadastro',methods=['GET','POST'])
 def cadastro():
     if request.method=='GET':
         return render_template('cadastro.html')
     email=request.form['email']
+    verificação=database.verificar_se_usuario_existe(email)
+    if verificação:
+        return "Você não pode cadastrar um email que já esta em uso"
+    senha=request.form['senha']
     nome=request.form['nome']
     senha=generate_password_hash(request.form['senha'])
     database.cadastro(email,nome,senha)
-    return redirect('/login.html')
+    return redirect(url_for('login'))
 
-@app.route
+@app.route('/login',methods=["GET","POST"])
+def login():
+    if request.method=="GET":
+        return render_template('login.html')
+    email=request.form['email']
+    verificação=database.verificar_se_usuario_existe(email)
+    if not verificação:
+        return "Algo deu de errado, tente novamente"
+    senha=request.form['senha']
+    listadobanco=database.pegar_senha(email)
+    if not check_password_hash(listadobanco[0],senha):
+        return 'pode nao'
+    session['email']=request.form['email']
+    return redirect(url_for('home'))
+
+@app.route('/home',methods=["GET"])
+def home():
+    musicas=database.pegar_musicas(session['email'])
+    return render_template('home.html',musicas=musicas)
+
+@app.route('/novo',methods=["GET","POST"])
+def novo():
+    if request.method=="GET":
+        return render_template ('novo.html',)
+    nome=request.form['nome']
+    artista=request.form['artista']
+    letra=request.form['letra']
+    status=request.form['status']
+    database.adicionar_musica(nome,artista,status,letra,session['email'])
+    return redirect(url_for('home'))
     
+
     
-    
+   
     
 
 
-# parte principal do
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import app
 
 def conectar_banco():
     conexao=sqlite3.connect("musica.db")
@@ -8,8 +9,8 @@ def criar_tabelas():
     conexao = conectar_banco()
     cursor = conexao.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (email TEXT PRIMARY KEY,nome TEXT , senha TEXT )''')
-    cursor.execute('''CREATE TABLE IF NOT EXISTS musicas (id INTEGER PRIMARY KEY, artista TEXT,letra TEXT, 
-                   email_usuario text, FOREIGN KEY (email_usuario) REFERENCES usuario(email))''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS musicas (id INTEGER PRIMARY KEY,nome TEXT, artista TEXT, status TEXT,
+                   letra TEXT, email_usuario text, FOREIGN KEY (email_usuario) REFERENCES usuario(email))''')
     conexao.commit()
     cursor.close()
 
@@ -19,7 +20,38 @@ def cadastro(email,nome,senha):
     cursor.execute('''INSERT INTO usuarios (email,nome,senha) VALUES (?,?,?)''',(email,nome,senha))
     conexao.commit()
     cursor.close()
+
+def pegar_senha(email):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute(''' SELECT senha FROM usuarios WHERE email=?''',(email,))
+    conexao.commit()
+    return cursor.fetchone()
+
+def verificar_se_usuario_existe(email):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''SELECT * FROM usuarios WHERE email=?''',(email,))
+    conexao.commit()
+    return cursor.fetchall()
+
+def adicionar_musica(nome,artista,status,letra,email):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    if nome=="1" and artista=="1" and status=="1" and letra=="1":
+        return "pode nao"
+    cursor.execute('''INSERT INTO musicas (nome,artista,status,letra,email_usuario) VALUES(?,?,?,?,?)''',(nome,artista,status,letra,email))
+    conexao.commit()
+    cursor.close()
+
+def pegar_musicas(email):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''SELECT * FROM musicas WHERE email_usuario=?''',(email,))
+    conexao.commit()
+    return cursor.fetchall()
     
+
     
 if __name__=="__main__":
     criar_tabelas()
